23a8117fd82e283e581c373a837043b65ddb01e2
Diorsevelt <yabadidap@gmail.com>
2026-03-26 08:54:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import json
+
+x = {
+    "Personalites": ["INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP"],
+    "No.Questions": 30,
+    "Qualities": ["Introvert", "Extrovert", "Sensing", "Intuition", "Thinking", "Feeling", "Prospecting", "Judging"]
+}
+
+
